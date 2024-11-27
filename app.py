d36b71fa0720ad6362f31acbb2059af9bed62428
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-11-27 11:26:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ feature_names = load_wine().feature_names
 st.title('Bienvenue !')
 
 df = pd.DataFrame(values, columns=feature_names)
-
 df
+plot = df['alcohol']
+st.write('Voici le grafique de la colonne alcohol: ')
+st.line_chart(plot)
+
+
 
